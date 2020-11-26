ff175f4a992ab0396af016ddd18c778f5714b590
Joe Thompson <joseph.thompson>
2020-11-26 21:03:25 +0000

    added build process script

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,27 @@
 const uglify = require('uglifyjs-webpack-plugin');
 const path = require("path");
+const analyser = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (env) => {
     return {
         mode: env.mode === "production" ? "production" : "development",
-        devtool: 'inline-source-map',
+        devtool: env.mode === "production" ? undefined : 'inline-source-map',
         entry: './src/index.tsx',
         output: {
             filename: 'bundle.js',
-            path: __dirname + '/dist',
+            path: __dirname + '/build',
             publicPath: '/output'
         },
-        optimization: {
-            minimizer: [
-                new uglify({
-                    uglifyOptions: {
-                        compress: {
-                            drop_console: true,
-                        },
-                        output: {
-                            comments: false
-                        }
-                    }
-                })
-            ]
-        },
+        plugins: [
+            new analyser()
+        ],
         resolve: {
             extensions: ['.ts', '.tsx', '.js'],
             alias: {
                 "@config": path.join(__dirname, "src", "config", env.mode === "production" ? "live": "local"),
-                "@src": path.join(__dirname, "src")
+                "@src": path.join(__dirname, "src"),
+                "react": "preact/compat",
+                "react-dom": "preact/compat"
             }
         },
         module: {
